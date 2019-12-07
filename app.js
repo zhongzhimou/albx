f3a6e2c9441eb319f111dc554d83ef28634295d5
zhongzhimou <373931936@qq.com>
2019-12-07 10:20:37 +0800

    实现ejs抽离、render渲染

--- app.js
+++ app.js
@@ -8,14 +8,40 @@ const app = express()
 app.listen(3000,() =>{
     console.log('http://127.0.0.1:3000');
     })
+
+//5.托管静态资源
+app.use('/assets', express.static('assets'))
+//5.1图片资源托管
+app.use('/uploads', express.static('uploads'))
+
+//6.配置ejs模板引擎：设置当前node服务器使用ejs模板引擎
+app.set('view engine','ejs')
+//7.下面这句代码是进行ejs模板文件查询的默认目录配置、下面这句代码写完之后，以后所有的ejs资源都会指定__dirname+"views"
+app.set('views',__dirname+'/views')
+
 //4.添加路由配置
 app.get('/',(req,res)=>{
-    fs.readFile('./views/index.ejs', (err, data) => {
-        if (err){
-            res.end('404')
-        }else{
-            res.end('data')
-        }
-    })
+ // fs.readFile('./views/index.ejs', (err, data) => {
+ //     if (err){
+ //         res.end('404')
+ //     }else{
+ //         res.end(data)
+ //     }
+ // }) 由于使用6-7直接简写为以下代码
+    //render一共做了3件事情
+    //1.读取文件（读取了默认目录配置下的文件）
+    //2.实现渲染，现在暂无数据参与，不使用ejs渲染，使用前后端分离的做法
+    //3.返回渲染结果
+    res.render('index.ejs')
+})
+app.get('/admin',(req,res)=>{
+    // fs.readFile('./views/admin/index.ejs', (err, data) => {
+    //     if (err){
+    //         res.end('404')
+    //     }else{
+    //         res.end(data)
+    //     }
+    // }) 由于使用6-7直接简写为以下代码
+    res.render('admin/index.ejs')
 })
 
